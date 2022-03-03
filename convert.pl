d2547223dbe6d09e5fa96ce42b66c7909e29f917
Ziris85 <ziris85@live.com>
2022-03-02 20:08:21 -0500

    autopay; perlcritic; separator fix Fixed autopay detection & code numbers More readabe vars instead of $_ everywhere Implemented some perlcritic suggestions Remove trailing ; - apparently homebank doesn't like it (anymore?)

--- convert.pl
+++ convert.pl
@@ -7,7 +7,7 @@
 #         Author: Ziris85
 #       Homepage: https://github.com/Ziris85/HomeBank-CSV-Converter
 #        License: GPLv3.0
-#        Version: 0.1
+#        Version: 0.2
 #
 ###########################################
 
@@ -15,6 +15,7 @@ use strict;
 use warnings;
 use Getopt::Long;
 use Text::CSV;
+use 5.012;
 
 my ($incsv,$outcsv,$csv);
 my @arrcsv;
@@ -27,7 +28,7 @@ if ( ! $ARGV[0] || ! -f $ARGV[0]) {
 }
 
 ## Define our hash array for arguments, with some defaults defined
-my %col = (sep => ",",header => "n",date => "",payment => "",info => "",payee => "",memo => "",amount => "",category => "",tags => "");
+my %col = (sep => ",",header => "n",date => "",payment => "",info => "",payee => "",memo => "",amount => "",category => "",tags => "",autopay => "");
 
 ## Default to interactive mode, unless additional arguments detected
 if (@ARGV) {
@@ -84,77 +85,85 @@ sub convert() {
     $csv ||= Text::CSV->new({ sep_char => "$col{'sep'}"});
     if (! @arrcsv) {
         ## Read file into array (if not already done earlier)
-        open (INCSV, "<", $incsv) or die "Could not open '$incsv': $!\n";
-        @arrcsv = <INCSV>;
-        close INCSV;
+        open (my $infh, "<", $incsv) or die "Could not open '$incsv': $!\n";
+        @arrcsv = <$infh>;
+        close $infh;
     }
     if ($col{'header'} eq "y") {
         ## Drop header
         shift @arrcsv;
     }
-    foreach ( @arrcsv ){
+    foreach my $csvline ( @arrcsv ){
         ## Loop through array of contents of CSV file
-        if ($csv->parse($_)) {
+        if ($csv->parse($csvline)) {
             my @split = $csv->fields();
             my $line = "";
             if ($col{'autopay'} =~ /\d/) {
                 ## User provided a payment code, so set ALL transactions to this code
                 ## But first, is the code valid? If greater than 10 or less than 0, then no
                 if ($col{'autopay'} < 0 || $col{'autopay'} > 10) {
-                    print "Invalid payment code provided! Please see README for valid codes\n";
+                    warn "Invalid payment code provided! Please see README for valid codes";
                     exit;
                 }
             }
-            foreach ('date','payment','info','payee','memo','amount','category','tags') {
+            foreach my $column ('date','payment','info','payee','memo','amount','category','tags') {
                 ## Loop through each of the Homebank columns for building our new line
-                if ($_ eq "payment" && $col{'autopay'}) {
-                    ## On payment column and autopay requested. Do this instead of read from array
+                if ($column eq "payment") {
+                    ## On payment column. Detect if autopay is present
                     if ($col{'autopay'} =~ /\d/) {
                         ## Autopay code given, so just push to line
                         $line = "$line\"$col{'autopay'}\";";
-                    } elsif ($col{'autopay'} eq "y") {
+                    } elsif (lc($col{'autopay'}) eq "y") {
                         ## Autopay enabled
                         if ($col{'memo'}) {
                             ## Required memo column provided, so pass to detectCode subroutine and push result to line
                             my $paycode = &detectCode($split[$col{'memo'}]);
                             $line = "$line\"$paycode\";";
                         } else {
-                            print "Autopay requested, but no memo column specified (required for this).\nPlease provide memo column to use this feature. Exiting...\n";
+                            warn "Autopay requested, but no memo column specified (required for this).\nPlease provide memo column to use this feature. Exiting...";
                             exit;
                         }
+                    } elsif ($col{$column} =~ /\d/) {
+                        ## Column number provided, so push contents of array to line
+                        $line = "$line\"$split[$col{$column}]\";";
+                    } else {
+                        ## Column number not provided, so just insert empty quotes
+                        $line = "$line\"\";";
                     }
-                } elsif ($col{$_} =~ /\d/) {
+                } elsif ($col{$column} =~ /\d/) {
                     ## Column number provided, so push contents of array to line
-                    $line = "$line\"$split[$col{$_}]\";";
+                    $line = "$line\"$split[$col{$column}]\";";
                 } else {
                     ## Column number not provided, so just insert empty quotes
                     $line = "$line\"\";";
                 }
             }
+            $line =~ s/;$//;
             ## Push completed line to homebank array
             push(@homebank,$line);
         } else {
             ## Line in array couldn't be read
-            warn "Line could not be parsed: $_\n";
+            warn "Line could not be parsed: $csvline\n";
         }
     }
     ## Finished looping through contents of input file array. Now push converted array to output file
-    open(OUTCSV,'>',$outcsv) or die "Could not open '$outcsv' for writing: $!\n";
+    open(my $outfh,'>',$outcsv) or die "Could not open '$outcsv' for writing: $!\n";
     foreach (@homebank) {
-        print OUTCSV "$_\n";
+        say $outfh "$_";
     }
-    close OUTCSV;
-    print "Conversion finished! Please review results for accuracy.\n";
+    close $outfh;
+    say "Conversion finished! Please review results for accuracy.";
+    return
 }
 
 sub interactive() {
     ## Welcome to interactive mode!
     my ($head,$opt) = ("","r");
-    print "Welcome to the Homebank CSV converter script!\n\nThis will attempt to guide you through defining the columns in your CSV file\nto equivalent columns that Homebank recognizes.\n\n";
+    say "Welcome to the Homebank CSV converter script!\n\nThis will attempt to guide you through defining the columns in your CSV file\nto equivalent columns that Homebank recognizes.\n";
     ## Read file into array
-	open (INCSV, "<", $incsv) or die "Could not open '$incsv': $!\n";
-    @arrcsv = <INCSV>;
-    close INCSV;
+	open (my $infh, "<", $incsv) or die "Could not open '$incsv': $!\n";
+    @arrcsv = <$infh>;
+    close $infh;
     ## Ask user if their CSV file has a header
     print "Firstly, does your CSV file have a header? (press enter for default: n ) ";
     chomp($col{'header'} = lc(<STDIN>));
@@ -177,15 +186,15 @@ sub interactive() {
         ## Reading confirmed, so split line to elements
         my @split = $csv->fields();
         ## Now ask user if we read it correctly by showing number and contents of columns
-        print "\nI found " . scalar @split . " columns, with the following information:\n";
+        say "\nI found " . scalar @split . " columns, with the following information:";
         foreach (@split) {
-            print " - $_\n";
+            say " - $_";
         }
         print "Does that look correct? (Y/n) ";
         chomp(my $cont = lc(<STDIN>));
         if ($cont eq "y") {
             ## User confirmed read properly
-            print "\nGood! Now we need to assign each of these to Homebank-equivalent columns.\nNot every column needs to be defined,\nand not every column in your CSV may have an equivalent.\nFor each column, do your best to choose an equivalent,\nor 'None' to skip it and omit its data from the output file\n";
+            say "\nGood! Now we need to assign each of these to Homebank-equivalent columns.\nNot every column needs to be defined,\nand not every column in your CSV may have an equivalent.\nFor each column, do your best to choose an equivalent,\nor 'None' to skip it and omit its data from the output file";
             ## Now, populate new hash array with header data to help user choose which match Homebank-equivalent columns
             my $i = 1;
             my %list = (0 => "None");
@@ -194,10 +203,10 @@ sub interactive() {
                 ++$i;
             }
             ## Looping through each Homebank column
-            foreach ('date','payment','info','payee','memo','amount','category','tags') {
+            foreach my $column ('date','payment','info','payee','memo','amount','category','tags') {
                 ## Payment column, ask if user wishes to use autopay
-                if ($_ eq "payment") {
-                    print "Enable automatic detection of payment codes? Or set all transactions to a single code?\nSee README for limitations and caveats of this feature ";
+                if ($column eq "payment") {
+                    print "Enable automatic detection of payment codes? Or set all transactions to a single code? (Y/#) \nSee README for limitations and caveats of this feature ";
                     chomp(my $resp = lc(<STDIN>));
                     if ($resp eq "y" || $resp =~ /\d/) {
                         $col{'autopay'} = $resp;
@@ -210,27 +219,28 @@ sub interactive() {
                 }
                 ## Loop to require an integer that also hasn't already been selected
                 while ($opt !~ /\d/ || ! $list{$opt}) {
-                    print "Which of the following columns contains '$_' info? ";
+                    print "Which of the following columns contains '$column' info? ";
                     chomp($opt = <STDIN>);
                 }
                 ## Unless None option selected (in which data in that column will be skipped in the output file)
                 ## remove the selected option from the header array and set the column number
                 unless ($opt == 0) {
-                    $col{$_}=$opt-1;
+                    $col{$column}=$opt-1;
                     delete $list{$opt};
                 }
                 ## Redefine opt var so input loop works on next iteration
                 $opt="r";
             }
             ## All finished with user input and column definitions. Begin conversion
-            print "All set! Conversion starting now...\n";
+            say "All set! Conversion starting now...";
             &convert();
         }
     } else {
         ## Can't read data with that separator
-        print "That separator doesn't appear to work. Please try again.\n";
+        say "That separator doesn't appear to work. Please try again.";
         exit;
     }
+    return
 }
 sub detectCode() {
     ## Some regex fun trying to determine what paycode might best define contents of memo
@@ -238,21 +248,21 @@ sub detectCode() {
     if ($memo =~ /(trans|x)fer/i) {
         return "4";
     } elsif ($memo =~ /bill\s?pay/i) {
-        return "10";
+        return "11";
     } elsif ($memo =~ /payment/i) {
-        return "7";
-    } elsif ($memo =~ /direct dep|deposit|((counter|teller) )?credit|interest/i) {
         return "8";
+    } elsif ($memo =~ /direct dep|deposit|((counter|teller) )?credit|interest/i) {
+        return "9";
     } elsif ($memo =~ /withdrawal|debit card/i) {
-        return "5";
+        return "4";
     } elsif ($memo =~ /(che(que|ck)|chk)(\s?[0-9]+)?/i) {
         return "2";
     } elsif ($memo =~ /debit card/i) {
-        return "5";
+        return "4";
     } elsif ($memo =~ /cash/i) {
         return "3";
     } elsif ($memo =~ /fee/i) {
-        return "9";
+        return "10";
     } else {
         return "0";
     }
